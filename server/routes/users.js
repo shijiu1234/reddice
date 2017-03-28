e32e4c550787283386bdbe8eb27d19f6845b875f
shijiu1234 <sjshijiu@qq.com>
2017-03-28 10:17:03 +0800

    Adds client-side validation. Extracts TextFieldGroup

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,39 +1,8 @@
 import express from 'express';
-import Validator from 'validator';
-import isEmpty from 'lodash/isEmpty';
+import validateInput from '../shared/validations/signup';
 
 let router = express.Router();
 
-function validateInput(data) {
-    let errors = {};
-
-    if (Validator.isEmpty(data.username)) {
-        errors.username = 'This field is required';
-    }
-    if (Validator.isEmpty(data.email)) {
-        errors.email = 'This field is required';
-    }
-    if (!Validator.isEmail(data.email)) {
-        errors.email = 'Email is invalid';
-    }
-    if (Validator.isEmpty(data.password)) {
-        errors.password = 'This field is required';
-    }
-    if (Validator.isEmpty(data.passwordConfirmation)) {
-        errors.passwordConfirmation = 'This field is required';
-    }
-    if (!Validator.equals(data.passwordConfirmation, data.password)) {
-        errors.passwordConfirmation = 'Passwords must match';
-    }
-    if (Validator.isEmpty(data.timezone)) {
-        errors.timezone = 'This field is required';
-    }
-
-    return {
-        errors,
-        isValid: isEmpty(errors)
-    }
-}
 
 router.post('/', (req, res) => {
     const {errors, isValid} = validateInput(req.body);
