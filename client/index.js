a9597c2f77f3f9c83d2a87ea72f394ff8d837a60
shijiu1234 <sjshijiu@qq.com>
2017-03-28 08:39:13 +0800

    Sends data to server via redux thunk action

--- client/index.js
+++ client/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { render } from 'react-dom';
-import {BrowserRouter as Router, Route } from 'react-router-dom';
+import {render} from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
 import Greetings from './components/Greetings';
 import SignupPage from './components/signup/SignupPage';
 
+const store = createStore(
+    (state = {}) => state,
+    applyMiddleware(thunk)
+);
+
 render(
-    <Router>
-        <div>
-            <Route path="/" component={App} />
-            <Route exact path="/" component={Greetings} />
-            <Route path="/signup" component={SignupPage} />
-        </div>
-    </Router>, document.getElementById('app')
+    <Provider store={store}>
+        <Router>
+            <div>
+                <Route path="/" component={App}/>
+                <Route exact path="/" component={Greetings}/>
+                <Route path="/signup" component={SignupPage}/>
+            </div>
+        </Router>
+    </Provider>
+    , document.getElementById('app')
 );
